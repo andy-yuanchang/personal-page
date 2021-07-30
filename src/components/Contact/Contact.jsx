1b10d9418724f56ac3e92ba204ae791d001221f5
andy <andy.yuanchang@gmail.com>
2021-07-30 18:58:15 +0800

    Implement Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,6 +8,10 @@ function Contact(props) {
   const titleRef = useRef(null);
   const isIntersect = useOnScreen(titleRef)
 
+  const handleSubmit = (e) => {
+    
+  }
+
   return (
     <div className="contact">
       <h1
@@ -16,7 +20,21 @@ function Contact(props) {
       >
         React Out to Me
       </h1>
-      <form action=""></form>
+      <form 
+        className="form-control" 
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" type="text" />
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email"/>
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" rows="4" />
+        <input 
+          type="submit"
+          value="Send"
+        />
+      </form>
     </div>
   )
 }
